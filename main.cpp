962d2c2f5b988e0ac7c8a6e13fa14bcee24fb9a7
Don't specify <63457210+nexies@users.noreply.github.com>
2024-04-24 18:47:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,9 @@
 #include <QApplication>
-#include "second_try.h"
 #include "mainwindow.h"
-#include <QDebug>
-
-using namespace std;
-
-
 
 int main(int argc, char ** argv)
 {
     QApplication a(argc, argv);
-
-//    pnt rec6x10 [60];
-//    for(int i = 0; i < 10; i++)
-//    {
-//        for(int j = 0; j < 6; j++)
-//        {
-//            rec6x10[i*6 + j].x = i;
-//            rec6x10[i*6 + j].y = j;
-//        }
-//    }
-//    for(int i = 0; i < 60; i++)
-//    {
-//        qDebug() << rec6x10[i].x << rec6x10[i].y;
-//    }
-
-//    PentaminoSolver solver(rec6x10);
-//    if(solver.solution())
-//        solver.display_solution();
 
     MainWindow w;
     w.show();
